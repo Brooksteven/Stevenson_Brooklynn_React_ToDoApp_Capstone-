e4dd7a9ad0840bbce444e5988eb9042740004d7b
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-05-13 10:03:25 -0400

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+
+
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        {/* here is where we make our individual routes */}
+        <Route path='/' element={< />}/>
+      </Routes>
+    </>
+  )
+}
